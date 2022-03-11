814ce483821a13d2c963a5d758762acdae39f134
deviniciuss <vinicius_rosa98@outlook.com>
2022-03-11 20:42:49 +0000

    Function loop & moveFundo(Game Loop)

--- js/script.js
+++ js/script.js
@@ -6,4 +6,20 @@ function start(){
     $("#fundoGame").append("<div id='inimigo2'></div>");
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
+    //Main class of the game
+    var jogo = {}
+
+    //Game Loop
+    jogo.timer = setInterval(loop,30);
+
+    function loop(){
+        moveFundo();
+    }
+
+    function moveFundo(){
+        esquerda = parseInt($("#fundoGame").css("background-position"));
+	    $("#fundoGame").css("background-position",esquerda-1);
+    }
+
+
 }
