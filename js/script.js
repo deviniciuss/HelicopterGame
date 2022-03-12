a1b75a65c7d7c7d17a638420784b921f9549290b
deviniciuss <vinicius_rosa98@outlook.com>
2022-03-12 08:28:42 +0000

    Teclas and function moveJogador

--- js/script.js
+++ js/script.js
@@ -6,14 +6,30 @@ function start(){
     $("#fundoGame").append("<div id='inimigo2'></div>");
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
-    //Main class of the game
+    //Main variables of the game
     var jogo = {}
+    var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68
+    }
+    jogo.pressionou = [];
+
+    //Check if the user press some button
+    $(document).keydown(function(e){
+        jogo.pressionou[e.which] = true;
+    }); 
+    $(document).keyup(function(e){
+        jogo.pressionou[e.which] = false;
+    }); 
+
 
     //Game Loop
     jogo.timer = setInterval(loop,30);
 
     function loop(){
         moveFundo();
+        moveJogador();
     }
 
     function moveFundo(){
@@ -21,5 +37,19 @@ function start(){
 	    $("#fundoGame").css("background-position",esquerda-1);
     }
 
+    function moveJogador(){
+        if(jogo.pressionou[TECLA.W]){
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo-10);
+        }
+        if(jogo.pressionou[TECLA.S]){
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo+10);
+        }
+        if(jogo.pressionou[TECLA.D]){
+            
+        }
+    
+    }
 
 }
