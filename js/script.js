ad77a5c75adccebada3aa172e77f1dd91d9523f4
deviniciuss <vinicius_rosa98@outlook.com>
2022-03-12 08:35:44 +0000

    Helicopter movement limits

--- js/script.js
+++ js/script.js
@@ -41,11 +41,22 @@ function start(){
         if(jogo.pressionou[TECLA.W]){
             var topo = parseInt($("#jogador").css("top"));
             $("#jogador").css("top", topo-10);
+                if(topo<=10){
+                    $("#jogador").css("top", topo+10);
+                }
+
+
         }
         if(jogo.pressionou[TECLA.S]){
             var topo = parseInt($("#jogador").css("top"));
             $("#jogador").css("top", topo+10);
+                if(topo>=434){
+                    $("#jogador").css("top", topo-10);
+                 }
+
+
         }
+
         if(jogo.pressionou[TECLA.D]){
             
         }
